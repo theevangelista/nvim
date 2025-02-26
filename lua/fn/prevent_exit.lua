0b9b11f5e9685cc67e1bbd220f533cf74298069b
Evangelista <evangelistajoaop@gmail.com>
2025-02-26 01:09:38 -0300

    remove prevent exit for now since it doesnt work

--- lua/fn/prevent_exit.lua
+++
@@ -1,8 +0,0 @@
-vim.api.nvim_create_autocmd("VimLeavePre", {
-	callback = function()
-		local choice = vim.fn.confirm("Exit Neovim?", "&Yes\n&No", 2)
-		if choice == 1 then
-			vim.cmd("qa")
-		end
-	end,
-})
